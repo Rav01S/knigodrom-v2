c775280bbd66174afd1c5b8789cb20b868ead381
Rav01S <ravil05safydinov@gmail.com>
2025-07-17 18:36:48 +0500

    Suspense reset password form

--- src/app/(pages)/auth/reset-password/page.tsx
+++ src/app/(pages)/auth/reset-password/page.tsx
@@ -1,7 +1,12 @@
 "use server";
 
 import ResetPasswordForm from "@/features/auth/ui/reset-password-form";
+import { Suspense } from "react";
 
 export default async function ResetPasswordPage() {
-  return <ResetPasswordForm />;
+  return (
+    <Suspense>
+      <ResetPasswordForm />
+    </Suspense>
+  );
 }
